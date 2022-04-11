e02dfcf3463c108279aaf32a00109cb3a3dd696d
kiran <kiran.sunway@gmail.com>
2022-04-11 10:15:29 +0545

    laravel first commit

---
+++ app/Http/Controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Home extends Controller
+{
+    function index(){
+        echo "Home controller is called";
+    }
+}
